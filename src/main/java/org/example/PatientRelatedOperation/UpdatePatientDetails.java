a0d74c2009ac4dcbb7af0d9480f296a16d692428
parasbramhankar <parasbramhankar02@gmail.com>
2025-07-01 19:35:41 +0530

    healthcare project commit

--- src/main/java/org/example/PatientRelatedOperation/UpdatePatientDetails.java
+++ src/main/java/org/example/PatientRelatedOperation/UpdatePatientDetails.java
@@ -1,6 +1,38 @@
 package org.example.PatientRelatedOperation;
 
+import org.example.ConnectionDetails;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class UpdatePatientDetails {
+     Scanner scanner=new Scanner(System.in);
 
+     public void updatePatDetails(){
+     String str="update patient email=? where patientId=?";
 
+     try {
+      Connection connection = ConnectionDetails.establishConnection();
+
+      PreparedStatement preparedStatement = connection.prepareStatement(str);
+
+      System.out.println("Enter Patient Id: ");
+      preparedStatement.setInt(2, scanner.nextInt());
+
+      System.out.println("Enter Email: ");
+      preparedStatement.setString(1, scanner.next());
+
+      int n = preparedStatement.executeUpdate();
+
+      if (n == 0) {
+          System.out.println("Row updated Successfully");
+      }
+  }catch (Exception e){
+      System.out.println(e.getMessage());
+  }
+
+ }
 }
--- src/main/java/org/example/PatientRelatedOperation/ViewPatientDetails.java
+++ src/main/java/org/example/PatientRelatedOperation/ViewPatientDetails.java
@@ -1,5 +1,73 @@
 package org.example.PatientRelatedOperation;
 
+import org.example.ConnectionDetails;
+import org.example.Patient;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class ViewPatientDetails {
 
+    public void viewAllPatientDetails() throws IOException, SQLException {
+
+        String string="select * from patient";
+
+        try{
+            Connection connection=ConnectionDetails.establishConnection();
+
+            PreparedStatement preparedStatement=connection.prepareStatement(string);
+
+            ResultSet resultSet= preparedStatement.executeQuery();
+
+            while (resultSet.next()){
+
+                Patient patient=new Patient(resultSet.getInt("patientId"),resultSet.getString("name"),resultSet.getString("gender"),
+                        resultSet.getInt("age"),resultSet.getString("phone"),resultSet.getString("email"),resultSet.getString("address"));
+
+                System.out.println(patient);
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+
+    }
+
+    public void viewPatientDetailsByName(){
+
+        String str="select * from patient where name=?";
+
+        try{
+            Connection connection=ConnectionDetails.establishConnection();
+
+            PreparedStatement preparedStatement=connection.prepareStatement(str);
+
+            // 3. Take input from user
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter patient ID: ");
+            preparedStatement.setInt(1, scanner.nextInt());
+
+            // 4. Execute query
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while(resultSet.next()){
+
+                Patient patient=new Patient(resultSet.getInt("patientId"),resultSet.getString("name"),resultSet.getString("gender"),
+                        resultSet.getInt("age"),resultSet.getString("phone"),resultSet.getString("email"),resultSet.getString("address"));
+
+                System.out.println(patient);
+
+            }
+
+            System.out.println(resultSet);
+
+        } catch (IOException | SQLException e) {
+            System.out.println("Message: "+e.getMessage());
+        }
+    }
 }
