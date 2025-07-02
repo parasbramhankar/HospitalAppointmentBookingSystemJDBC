846eb84bd040e2e29d443776be33b3a98842d3b7
parasbramhankar <parasbramhankar02@gmail.com>
2025-07-02 16:14:19 +0530

    final commit

--- src/main/java/org/example/AppointmentRelatedOperations/ViewAllAppointment.java
+++ src/main/java/org/example/AppointmentRelatedOperations/ViewAllAppointment.java
@@ -6,36 +6,42 @@ import org.example.ConnectionDetails;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Statement;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class ViewAllAppointment {
 
-    public void displayAppointment(){
+    public void displayAppointment() {
+        String str = "SELECT * FROM appointment";
 
-        String str="Select * from appointment";
+        try {
+            Connection connection = ConnectionDetails.establishConnection();
 
-        try{
-            Connection connection= ConnectionDetails.establishConnection();
+            Statement statement = connection.createStatement();
 
-            PreparedStatement preparedStatement= connection.prepareStatement(str);
+            ResultSet resultSet = statement.executeQuery(str);
 
-            ResultSet resultSet= preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                int id = resultSet.getInt("appointmentId");
+                int doctorId = resultSet.getInt("doctorId");
+                int patientId = resultSet.getInt("patientId");
+                LocalDate date = resultSet.getDate("date").toLocalDate();
+                LocalTime time = resultSet.getTime("time").toLocalTime();
+                String status = resultSet.getString("status");
 
-            while(resultSet.next()){
-                int id=resultSet.getInt("appointmentId");
-                int doctorId=resultSet.getInt("doctorId");
-                int patientId= resultSet.getInt("patientId");
-                LocalDate date= resultSet.getDate("date").toLocalDate();
-                LocalTime time=resultSet.getTime("time").toLocalTime();
-                String status= resultSet.getString("status");
-
-                Appointment appointment=new Appointment(id,doctorId,patientId,date,time,status);
+                Appointment appointment = new Appointment(id, doctorId, patientId, date, time, status);
                 System.out.println(appointment);
             }
 
-        }catch (Exception e){
+            // Close resources (optional but recommended)
+            resultSet.close();
+            statement.close();
+            connection.close();
+
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
+
 }
